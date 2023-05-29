257214e2197e592c3bda70517614d59d1552e7ea
info <info@pc002>
2023-05-30 00:52:22 +0900

    fixed error class name: sample->sample001

---
+++ src/test/Sample001.java
@@ -0,0 +1,20 @@
+package test;
+
+
+public class Sample001 {
+	
+	
+
+
+	public Sample001() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+		System.out.print("print the java class");
+	}
+
+	public static void main(String[] args){
+		Sample001 sam = new Sample001();
+		
+		
+	}
+}
