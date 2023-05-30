e88f92f23981a23d6478439b8c9efa8d6b694e91
info <info@pc002>
2023-05-30 10:00:40 +0900

    teach

--- src/test/Sample001.java
+++ src/test/Sample001.java
@@ -9,7 +9,7 @@ public class Sample001 {
 	public Sample001() {
 		super();
 		// TODO 自動生成されたコンストラクター・スタブ
-		System.out.print("print the java class");
+		System.out.print(this.toString());
 	}
 
 	public static void main(String[] args){
